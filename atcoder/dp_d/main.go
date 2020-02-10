15b7b64a9b71ce5db2a93761e90a57ae6d345b52
k.nitta <mmchari.0228@gmail.com>
2020-02-11 02:06:39 +0900

    dp d

--- atcoder/dp_d/main.go
+++ atcoder/dp_d/main.go
@@ -17,18 +17,18 @@ func run() {
 	dp := make([][]int, n+1)
 	// dp[i][sum_w] = v の最大値
 	// dp[i][j] = i 番目までの品物を、重さを j 以下になるように選んだときの価値の最大値
-	// chmax(dp[i-1][sum_w], dp[i-1][sum_w - w[i]] + v[i])
+	// 品物を選ばない & 品物を選ぶ のふた通りで DP を更新する
 	dp[0] = make([]int, limit+1)
 	for i := 0; i < n; i++ {
 		w, v := readN(), readN()
 		dp[i+1] = make([]int, limit+1)
 		for sumW := 0; sumW <= limit; sumW++ {
+			// まずは普通に品物を選択せずに更新する
+			chmax(&dp[i+1][sumW], dp[i][sumW])
 			if sumW-w >= 0 {
-				// i 番目の重さ以上の j で、価値の最大値を選択する
+				// 品物を選択した場合で更新する
 				chmax(&dp[i+1][sumW], dp[i][sumW-w]+v)
 			}
-			// i 番目の品物は使えない
-			chmax(&dp[i+1][sumW], dp[i][sumW])
 		}
 	}
 
