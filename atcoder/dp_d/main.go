4c070f3a86d1b7660d48c6899049131cbdf3c76b
k.nitta <mmchari.0228@gmail.com>
2020-02-12 01:08:29 +0900

    dpD

--- atcoder/dp_d/main.go
+++ atcoder/dp_d/main.go
@@ -13,26 +13,28 @@ func main() {
 
 func run() {
 	setSpace()
-	n, limit := int(readN()), readN()
-	dp := make([][]int, n+1)
-	// dp[i][sum_w] = v の最大値
-	// dp[i][j] = i 番目までの品物を、重さを j 以下になるように選んだときの価値の最大値
-	// 品物を選ばない & 品物を選ぶ のふた通りで DP を更新する
-	dp[0] = make([]int, limit+1)
-	for i := 0; i < n; i++ {
+	N, W := int(readN()), readN()
+
+	dp := make([][]int, N+1)
+	// dp[i][sumW]
+	// i 番目まで品物を選んで、合計の重さが sumW のときの価値の最大値
+
+	dp[0] = make([]int, W+1)
+	for i := 0; i < N; i++ {
 		w, v := readN(), readN()
-		dp[i+1] = make([]int, limit+1)
-		for sumW := 0; sumW <= limit; sumW++ {
-			// まずは普通に品物を選択せずに更新する
-			chmax(&dp[i+1][sumW], dp[i][sumW])
+		dp[i+1] = make([]int, W+1)
+		// i + 1 番目の更新は、
+		for sumW := 0; sumW <= W; sumW++ {
+			// i 番目までの価値をそのまま更新する
+			dp[i+1][sumW] = dp[i][sumW]
 			if sumW-w >= 0 {
-				// 品物を選択した場合で更新する
+				// もし、選べるとしたら, 比較して、価値があがるのであれば更新する
 				chmax(&dp[i+1][sumW], dp[i][sumW-w]+v)
 			}
 		}
 	}
 
-	fmt.Println(dp[n][limit])
+	fmt.Println(dp[N][W])
 }
 
 func chmax(a *int, b int) {
